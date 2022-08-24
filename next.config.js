dc9687b868e7158bf2d5234088c0f392ff8b6a01
Papaito <eliana@iphone-2.local>
2022-08-24 09:34:36 -0600

    splash page working when browsing to /

--- next.config.js
+++ next.config.js
@@ -2,6 +2,16 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  rewrites: async () => {
+    return [
+      {
+        source: "/",
+        destination: "/splash.html",
+      }
+    ]
+  }
 }
+
+
 
 module.exports = nextConfig
